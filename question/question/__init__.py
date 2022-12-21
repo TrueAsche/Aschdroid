6c27293a9987417fdbf025dc62538e23f0657dc1
TrueAsche <102401126+TrueAsche@users.noreply.github.com>
2022-12-20 23:47:16 -0500

    Delete question/question directory

--- question/question/__init__.py
+++
@@ -1,8 +0,0 @@
-# This init is required for each cog.
-# Import your main class from the cog's folder.
-from .question import Question
-
-
-def setup(bot):
-    # Add the cog to the bot.
-    bot.add_cog(Question())
