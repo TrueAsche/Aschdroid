422a8bd8e382f2bdab0d46e2996816997ea4624d
TrueAsche <102401126+TrueAsche@users.noreply.github.com>
2022-12-20 23:47:49 -0500

    Add files via upload

---
+++ question/question.py
@@ -0,0 +1,15 @@
+from redbot.core import commands
+import time
+
+class Question(commands.Cog):
+    """Description of the cog visible with [p]help MyFirstCog"""
+
+    @commands.command()
+    async def question(self, ctx):
+        """Counts down from 7"""
+        i = 7
+        while(i > 0):
+            await ctx.send(i)
+            time.sleep(1)
+            i = i - 1
+        await ctx.send("Answer Window closed!")
